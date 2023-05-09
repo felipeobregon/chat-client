3c2938797966443786acee7a49f882c6f67d0fa7
felipe <felipe.a.obregon@gmail.com>
2023-05-09 13:55:04 -0400

    add main component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+'use client'
+
+import Parent from '../components/search'
+
 export default function Hello() {
     return (
         <div>
             <h1>hello </h1>
+            <Parent/>
         </div>
     )
 }

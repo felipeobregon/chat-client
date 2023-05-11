a716f5a5eab775bdc4591710f904f6c64590ee4d
felipe <felipe.a.obregon@gmail.com>
2023-05-11 12:35:47 -0400

    add a GET endpoint

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,6 +1,22 @@
 import { NextResponse } from 'next/server';
- 
-export async function GET() {
+import { Configuration, OpenAIApi } from 'openai'
 
-  return NextResponse.json({ 'hi': 'test' });
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+
+export async function GET() {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+      });
+
+  return NextResponse.json(response.data);
 }
